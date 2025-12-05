e52e741be23d432ab8e93e8013d99c24904e7deb
Adam Sporka <adam.j.sporka@gmail.com>
2025-12-05 04:17:24 +0100

    Works

---
+++ src/engine.h
@@ -0,0 +1,229 @@
+#pragma once
+
+#include <assert.h>
+#include <map>
+
+using PARAM_NAME = int;
+using PARAM_VALUE = float;
+
+////////////////////////////////////////////////////////////////
+class CGen
+{
+public:
+};
+
+////////////////////////////////////////////////////////////////
+class CGenSquareWave : public CGen
+{
+public:
+    int period = 0;
+    int duty_cycle = 0;
+    int counter = 0;
+
+    void setup(int period_, int duty_cycle_)
+    {
+        period = period_;
+        duty_cycle = duty_cycle_;
+    }
+
+    inline float getNextSample()
+    {
+        float out;
+        if (counter < duty_cycle)
+        {
+            out = .1f;
+        }
+        else
+        {
+            out = -.1f;
+        }
+        counter++;
+        if (counter == period) counter = 0;
+        return out;
+    }
+};
+
+////////////////////////////////////////////////////////////////
+class CGenSineWave : public CGen
+{
+public:
+    float phase;
+    float frequency;
+};
+
+////////////////////////////////////////////////////////////////
+class CEvent
+{
+public:
+    enum EVENT_STATE
+    {
+        NOT_PLAYING, // Initial state
+        UPCOMING, // Created but not put in a slot yet
+        PLAYING, // Producing sound
+        BEING_STOLEN, // Should be removed from its slot and killed
+        RELEASED, // Reported not being processed anymore
+    } m_State = NOT_PLAYING;
+    std::map<PARAM_NAME, PARAM_VALUE> m_Rtpc;
+    int64_t m_Served = 0;
+    CEvent* m_StolenBy = nullptr;
+
+    EVENT_STATE getState() { return m_State; }
+
+    void handleStealing()
+    {
+        if (m_State == BEING_STOLEN)
+        {
+            m_State = RELEASED;
+        }
+    }
+
+    // Interleaved LlRrLlRr ...
+    virtual void fillStereoBuffer(int16_t* output, int num_frames, int num_channels)
+    {
+        assert(num_channels == 2);
+        assert(num_frames > 0);
+
+        int num_samples = num_frames * num_channels;
+        auto* wrt = output;
+        for (int a = 0; a < num_samples; a++)
+        {
+            *wrt = rand() % 16384 - 8192;
+            wrt++;
+        }
+
+        handleStealing();
+    }
+};
+
+class CEventExplosion : public CEvent
+{
+    CGenSquareWave sqr;
+public:
+    CEventExplosion()
+    {
+        sqr.setup(30, 15);
+    }
+
+    // Interleaved LlRrLlRr ...
+    virtual void fillStereoBuffer(int16_t* output, int num_frames, int num_channels)
+    {
+        assert(num_channels == 2);
+        assert(num_frames > 0);
+
+        sqr.period += 2;
+        sqr.duty_cycle += 1;
+        int num_samples = num_frames * num_channels;
+        auto* wrt = output;
+        for (int a = 0; a < num_samples; a += 2)
+        {
+            float f = sqr.getNextSample();
+            *wrt = f * 32767;
+            wrt++;
+            *wrt = f * 32767;
+            wrt++;
+        }
+
+        if (sqr.period == 200)
+        {
+            m_State = EVENT_STATE::RELEASED;
+        }
+
+        handleStealing();
+    }
+};
+
+class CEventEngine : public CEvent
+{
+};
+
+////////////////////////////////////////////////////////////////
+#define NUM_EVENT_SLOTS 128
+class CEngine
+{
+private:
+    int16_t intermediate_buffer[1024 * 1024];
+
+public:
+    // Interleaved LlRrLlRr ...
+    void fillStereoBuffer(int16_t* output, int num_frames, int num_channels)
+    {
+        assert(num_channels == 2);
+        assert(num_frames > 0);
+
+        int num_samples = num_frames * num_channels;
+        memset(output, 0, num_samples * sizeof(int16_t));
+
+        for (int a = 0; a < NUM_EVENT_SLOTS; a++)
+        {
+            if (!events[a]) continue;
+
+            if (events[a]->m_State == CEvent::EVENT_STATE::PLAYING || events[a]->m_State == CEvent::EVENT_STATE::BEING_STOLEN)
+            {
+                events[a]->fillStereoBuffer(intermediate_buffer, num_frames, num_channels);
+                for (int i = 0; i < num_samples; i++)
+                {
+                    output[i] += intermediate_buffer[i];
+                }
+            }
+        }
+    }
+
+    CEngine()
+    {
+        memset(events, 0, sizeof(CEvent*) * NUM_EVENT_SLOTS);
+    }
+
+    void deleteReleasedEvents()
+    {
+        // It is safe to delete all RELEASED events;
+        for (int a = 0; a < NUM_EVENT_SLOTS; a++)
+        {
+            // This slot doesn't have anything in yet
+            if (!events[a]) continue;
+
+            // The audio thread has released this event --> can be deleted and forgotten
+            if (events[a]->getState() == CEvent::EVENT_STATE::RELEASED)
+            {
+                auto stolen_by = events[a]->m_StolenBy;
+                delete events[a];
+                events[a] = nullptr;
+                if (stolen_by)
+                {
+                    events[a] = stolen_by;
+                }
+            }
+        }
+    }
+
+    CEvent* createEvent(CEvent* new_event)
+    {
+        // Find a slot for it
+        for (int a = 0; a < NUM_EVENT_SLOTS; a++)
+        {
+            if (!events[a])
+            {
+                events[a] = new_event;
+                new_event->m_State = CEvent::EVENT_STATE::PLAYING;
+                return new_event;
+            }
+        }
+
+        // None found? Steal the oldest.
+        int64_t max_served = 0;
+        int candidate_slot = 0;
+        for (int a = 0; a < NUM_EVENT_SLOTS; a++)
+        {
+            if (events[a]->m_Served >= max_served)
+            {
+                max_served = events[a]->m_Served;
+                candidate_slot = a;
+            }
+        }
+
+        events[candidate_slot]->m_State = CEvent::EVENT_STATE::BEING_STOLEN;
+        events[candidate_slot]->m_StolenBy = new_event;
+    }
+
+private:
+    CEvent* events[NUM_EVENT_SLOTS];
+};
---
+++ src/main.cpp
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "windows.h"
+#include "engine.h"
+
+#define SAMPLE_RATE 48000
+#define NUM_CHANNELS 2
+#define BUFLEN 512
+
+int dummy_buffers = 0;
+HWAVEOUT hWaveOut;
+WAVEHDR whdr[8];
+unsigned char* wh_data[8];
+int round_robin = 0;
+
+CEngine engine;
+
+////////////////////////////////////////////////////////////////
+DWORD dw;
+void CALLBACK waveOutProc(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2)
+{
+    switch (uMsg) {
+    case WOM_OPEN:
+        break;
+    case WOM_CLOSE:
+        break;
+    case WOM_DONE:
+        engine.fillStereoBuffer((int16_t*)wh_data[round_robin % 4], BUFLEN, NUM_CHANNELS);
+        waveOutWrite(hWaveOut, &whdr[round_robin], sizeof(whdr[round_robin]));
+        round_robin = (round_robin + 1) & 0x3;
+        break;
+    default:
+        break;
+    }
+}
+
+void openAudio()
+{
+    // Create the output buffers
+    WAVEFORMATEX waveformatex;
+    waveformatex.wFormatTag = WAVE_FORMAT_PCM;
+    waveformatex.nChannels = NUM_CHANNELS;
+    waveformatex.nSamplesPerSec = static_cast<int>(SAMPLE_RATE);
+    waveformatex.nAvgBytesPerSec = static_cast<int>(SAMPLE_RATE * NUM_CHANNELS * 2); // 2 channels, 2 bytes per sample
+    waveformatex.nBlockAlign = NUM_CHANNELS * 2;
+    waveformatex.wBitsPerSample = 16;
+    waveformatex.cbSize = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        wh_data[i] = new unsigned char[BUFLEN * NUM_CHANNELS * 2];
+        memset(wh_data[i], 0, BUFLEN * NUM_CHANNELS * 2);
+        whdr[i].lpData = (char*)wh_data[i];
+        whdr[i].dwBufferLength = BUFLEN * NUM_CHANNELS * 2;
+        whdr[i].dwBytesRecorded = 0;
+        whdr[i].dwUser = 0;
+        whdr[i].dwFlags = 0;
+        whdr[i].dwLoops = 0;
+    }
+
+    // Open the sound card
+    waveOutOpen(&hWaveOut, WAVE_MAPPER, &waveformatex, (DWORD_PTR)waveOutProc, (DWORD_PTR)&dw, CALLBACK_FUNCTION | WAVE_FORMAT_DIRECT);
+
+    // Pre-buffer
+    for (int i = 0; i < 4; i++)
+    {
+        memset((int16_t*)wh_data[i], 0, sizeof(int16_t) * BUFLEN);
+        waveOutPrepareHeader(hWaveOut, &whdr[i], sizeof(whdr[i]));
+        waveOutWrite(hWaveOut, &whdr[i], sizeof(whdr[i]));
+    }
+}
+
+////////////////////////////////////////////////////////////////
+
+int main(int argc, char** argv[])
+{
+    openAudio();
+    char key;
+    do
+    {
+        key = _getch();
+        if (key == 'a')
+        {
+            engine.createEvent(new CEventExplosion());
+        }
+    }
+    while (key != 27);
+    printf("Terminating.");
+}
